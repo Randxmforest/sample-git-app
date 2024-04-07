e613023fd5aaa4c2ab6bf2b1e2788a6c2763e316
Randxm4est <avirukth@gmail.com>
2024-04-08 08:25:45 +1000

    initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+st.title("Random Ass Website")
+
+col1,col2=st.columns(2)
+
+with col1:
+    st.write('')
+
+
+
+
+
+
+with col2:
+    st.write("206 National Olympic Committee's (NOC) will compete at the Paris Games - each represents a particular country or region of the world. For each country/region, an identifying IOC code is recorded and also the name of the country/region and its population. Each NOC")
+
+st.header('Courses Added')
+st.subheader('Data Science')
+
+
+
+st.subheader('pyrhon')
+st.subheader('sql')
